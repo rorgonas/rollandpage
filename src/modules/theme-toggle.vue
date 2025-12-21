f9154d4d0dded83ac9714b10d2f5a805d49dbf51
Istvan Rolland Orgonas <rorgonas@gmail.com>
2025-12-21 17:09:48 -0500

    iro - save theme toggle template

---
+++ src/modules/theme-toggle.vue
@@ -0,0 +1,15 @@
+<template>
+  <button
+    class="theme-toggle"
+    :class="`theme-toggle--${size}`"
+    type="button"
+    :aria-label="isDark ? 'Switch to light mode' : 'Switch to dark mode'"
+    @click="toggleTheme"
+  >
+    <span v-if="!isDark" class="theme-toggle__icon" aria-hidden="true" v-html="moonSvgIcon"></span>
+    <span v-else class="theme-toggle__icon" aria-hidden="true" v-html="sunSvgIcon"></span>
+  </button>
+</template>
+
+<script lang="ts" src="./theme-toggle.ts"></script>
+<style scoped lang="css" src="./theme-toggle.css"></style>
